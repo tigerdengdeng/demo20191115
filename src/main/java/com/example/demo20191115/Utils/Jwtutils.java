1a7fcef5ff7d8b4333c5440ff9e44f0246b240e0
mytiger <2893171563@qq.com>
2019-12-05 14:07:40 +0800

    增加验证

--- src/main/java/com/example/demo20191115/Utils/Jwtutils.java
+++ src/main/java/com/example/demo20191115/Utils/Jwtutils.java
@@ -1,9 +1,17 @@
 package com.example.demo20191115.Utils;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 /*
 JWT验证类
  */
 public class Jwtutils {
+     public  static  final  String SUBJECT = "xdclass";
 
+     public  static  String geneJsonWebToken()
+     {
+          return null;
+
+     }
 
 }
